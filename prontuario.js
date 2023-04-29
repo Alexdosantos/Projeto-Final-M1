5b9e79faa9429847a419389576c044f571f27800
Alex dos Santos <125704045+Alexdosantos@users.noreply.github.com>
2023-04-29 11:01:47 -0300

    atualizado

--- prontuario.js
+++ prontuario.js
@@ -7,162 +7,75 @@ const id = parametros.get('id');
 async function carregarDadosPaciente() {
   try {
     const api = await fetch(`https://projeto-final-arnia.onrender.com/cadastro-pacientes/${id}`);
+    if (api.status === 404) {
+      console.log('Paciente não encontrado');
+      return;
+    }
     const paciente = await api.json();
-
     const nome = paciente.nome;
     const dataNasc = paciente.dataNascimento;
     const profissao = paciente.profissao;
     const ensino = paciente.ensino;
-
     // preencher os campos da página com os dados do paciente
     document.getElementById('nome-idPaciente').innerHTML = nome;
     document.getElementById('nasc-idPaciente').innerHTML = dataNasc;
     document.getElementById('profissao-idPaciente').innerHTML = profissao;
     document.getElementById('ensino-idPaciente').innerHTML = ensino;
-  } catch (error) {
-    console.log(error);
-  }    
-  
+  } catch (erro) {
+    console.log(erro);
+  }
 }
 
 carregarDadosPaciente();
 
-
-//  AQUI REALIZA O MÉTODO DE POST PARA SALVAR OS DADOS DA SESSÃO
+// realizar o método POST para salvar os dados da sessão
 const salveSessao = async () => {
-    try {
-      const api = await fetch('https://projeto-final-arnia.onrender.com/sessoesPaciente', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          data: document.querySelector('#data-newSessao').value,
-          horaInicio: document.querySelector('#time-InicioSessao').value,
-          horaFinal: document.querySelector('#time-FimSessao').value,
-          titulo: document.querySelector('#titulo-input').value,
-          resumo: document.querySelector('#tituloTextModal').value,
-          idPaciente: parseInt(id)
-        })
-      });
-      const response = await api.json();
-      console.log(response);
-    } catch (error) {
-      console.log(error);
-    }
-    //window.location.href = `prontuario.html?id=${id}`
-  };
-  
-  document.querySelector('#btn-criarNovasessao').addEventListener('click', salveSessao);
-  
-// AQUI REALIZA O MÁTODO POST PARA SALVA OS DADOS EM FOTO RELEVANTE
-  const salveFatoRelenate = async () => {
-    try {
-      const api = await fetch('https://projeto-final-arnia.onrender.com/fatoRelevante', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          data: document.querySelector('#dataFato').value,
-          titulo: document.querySelector('#tituloFato').value,
-          descricao: document.querySelector('#tituloFatoModal').value,
-          idPaciente: parseInt(id)
-        })
-      });
-      const response = await api.json();
-      console.log(response);
-    } catch (error) {
-      console.log(error);
-    }
-    //window.location.href = `prontuario.html?id=${id}`
-  };
-  
-  document.querySelector('#btn-criarFatoRelevante').addEventListener('click', salveFatoRelenate);
+  try {
+    const api = await fetch('https://projeto-final-arnia.onrender.com/sessoesPaciente', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        data: document.querySelector('#data-newSessao').value,
+        horaInicio: document.querySelector('#time-InicioSessao').value,
+        horaFinal: document.querySelector('#time-FimSessao').value,
+        titulo: document.querySelector('#titulo-input').value,
+        resumo: document.querySelector('#tituloTextModal').value,
+        idPaciente: parseInt(id)
+      })
+    });
+    const resposta = await api.json();
+    console.log(resposta);
+  } catch (erro) {
+    console.log(erro);
+  }
+  // window.location.href = `prontuario.html?id=${id}`;
+};
 
+document.querySelector('#btn-criarNovasessao').addEventListener('click', salveSessao);
 
+// realizar o método POST para salvar os dados em fato relevante
+const salveFatoRelevante = async () => {
+  try {
+    const api = await fetch('https://projeto-final-arnia.onrender.com/fatoRelevante', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        data: document.querySelector('#dataFato').value,
+        titulo: document.querySelector('#tituloFato').value,
+        descricao: document.querySelector('#tituloFatoModal').value,
+        idPaciente: parseInt(id)
+      })
+    });
+    const resposta = await api.json();
+    console.log(resposta);
+  } catch (erro) {
+    console.log(erro);
+  }
+  // window.location.href = `prontuario.html?id=${id}`;
+};
 
-
-
-const dadoSessao = async () =>  {
-    const postSessao = document.querySelector('#cardsessaonew')
-    const apiSessao = await fetch (` https://projeto-final-arnia.onrender.com/sessoesPaciente?idPaciente=${id}`)
-    const newdados =  await apiSessao.json()
-    let conteudo = ''
-    
-
-    newdados.forEach(news => {
-        
-        conteudo += `
-        <section id="section-cardSessao">
-        <img class="barraVerde" src="/assets/barra verde.svg" alt="">
-            <div> 
-                
-
-                <div class="cardSessao">
-                    <div class="corverde"></div>
-                    
-                    <img class="img1" src="/assets/logo sessão.svg" alt="">
-                    
-                        <h4>Sessão 01</h4>
-                        <button id="opcoes"><img src="/assets/3 pontos.svg"alt=""></button>
-
-                    
-                    
-                    <p class="date-sessao" id="date-sessao">${news.data}</p>
-                    <p class="textInfo-sessao" id="textInfo-sessao">${news.resumo}</p>
-                </div>
-            </div>
-            </section>
-
-        
-    `
-        
-    })
-    postSessao.innerHTML = conteudo
-    //window.location.href = `prontuario.html?id=${id}`
-    
-    
-    
-}
-
-window.addEventListener('DOMContentLoaded' , dadoSessao)
-
-
-const dadosfatoRelevante = async () =>  {
-   
-    const postfato = document.querySelector('#section-fatoRelevante')
-    const apifato = await fetch (` https://projeto-final-arnia.onrender.com/fatoRelevante?idPaciente=${id}`)
-    const newfato =  await apifato.json()
-    let conteudo = ''
-
-    newfato.forEach(news => {
-        conteudo += `<img class="retanguloAzul" src="/assets/Rectangle azul.svg" alt="">
-        
-                
-                
-        <div class="cardfatoRelevante">
-            <img class="img-fatorelevante" src="/assets/logo fato relevante.svg" alt="">
-            
-            <h4>Fato Relevante</h4>
-            <button id="opcoes1"><img src="/assets/3 pontos.svg"alt=""></button>
-            
-            
-            <p class="dateFatorelevante">${news.data}</p>
-            <p class="textInfo-FatoRelefante">${news.descricao}</p>
-        </div>`
-        
-    })
-    postfato.innerHTML = conteudo
-    
-    //window.location.href = `prontuario.html?id=${id}`
-}
-
-window.addEventListener('DOMContentLoaded' , dadosfatoRelevante)
-  
-
-function abrirSessaoValore(id) {
-  window.location.href = `sessao.html?id=${id}`
-
-}
-
+document.querySelector('#btn-criarFatoRelevante').addEventListener('click' , salveFatoRelevante)
